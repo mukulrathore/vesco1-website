7a6de72cabdb1073bf61646bf7ae794ea8f5dd17
adam890-nike <80244472+adam890-nike@users.noreply.github.com>
2021-06-23 10:20:34 +0530

    this is vesco

---
+++ js/custom.js
@@ -0,0 +1,127 @@
+/*=====================================
+               SERVICES
+=======================================*/
+$(document).ready(function(){
+    //animate on scroll
+    new WOW().init();
+});
+/*=====================================
+               WORK
+=======================================*/
+$(function(){
+    $("#work").magnificPopup({
+        delegate: 'a',
+        type:'image',
+        gallery: {
+            enabled:true
+        }
+    });
+});
+/*=====================================
+              TEAM
+=======================================*/
+$(function(){
+    $("#team-members").owlCarousel({
+        items:3,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            }
+        }
+    });
+
+});
+/*=====================================
+              Testimonials 
+=======================================*/
+
+$(function(){
+    $("#customers-testimonials").owlCarousel({
+        items:1,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoplayHoverPause:true
+    });
+
+});
+/*=====================================
+              STATS 
+=======================================*/
+$(function(){
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+    
+});
+/*=====================================
+              CLIENTS
+=======================================*/
+$(function(){
+    $("#clients-list").owlCarousel({
+        items:6,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoplayHoverPause:true,
+        responsive : {
+            0 : {
+                items:1
+            },
+            480 : { 
+                items:3
+            },
+            768 : {
+                items:5
+            },
+            992 : {
+                items:6
+            }
+        }
+    });
+
+});
+/*=====================================
+              NAVIGATION
+=======================================*/
+//show/hide transparent black navigation
+$(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop()<50){
+            $("nav").removeClass("vesco-top-nav");
+            $("#back-to-top").fadeOut();
+
+        }else{
+            $("nav").addClass("vesco-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
+//smooth scrolling 
+$(function(){
+    $("a.smooth-scroll").click(function(event){
+        event.preventDefault();
+        //get/return id like #about,#work,#team and etc.
+        var section=$(this).attr("href");
+        $('html,body').animate({
+            scrollTop:$(section).offset().top - 64
+        },1250,"easeInOutExpo");
+    });
+});
+//close mobile menu on click
+$(function(){
+    $(".navbar-collapse ul li a").on("click touch",function(){
+        $(".navbar-toggle").click();
+    });
+});
